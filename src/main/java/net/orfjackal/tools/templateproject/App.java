ee9fd9a8f2ee1f17bf5cc0a781de229d6bb9a22e
Esko Luontola <esko.luontola@gmail.com>
2008-02-28 20:39:21 +0000

    Initial project layout

---
+++ src/main/java/net/orfjackal/tools/templateproject/App.java
@@ -0,0 +1,12 @@
+package net.orfjackal.tools.templateproject;
+
+/**
+ * @author Esko Luontola
+ * @since 4.1.2008
+ */
+public class App {
+
+    public boolean ok() {
+        return true;
+    }
+}
