d1ec2659e56a8dec75aae409ee71ade4d8c6fd31
Esko Luontola <esko.luontola@gmail.com>
2008-02-29 19:13:34 +0000

    WorkDirectoryManager

---
+++ src/main/java/net/orfjackal/pommac/WorkDirectoryManager.java
@@ -0,0 +1,53 @@
+package net.orfjackal.pommac;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Esko Luontola
+ * @since 29.2.2008
+ */
+public class WorkDirectoryManager {
+
+    private static int counter = 0;
+
+    private final List<File> createdDirs = new ArrayList<File>();
+    private final String prefix = "temp_" + System.currentTimeMillis() + "_";
+    private final File parentDir;
+
+    public WorkDirectoryManager(File parentDir) {
+        assert parentDir.isDirectory();
+        this.parentDir = parentDir;
+    }
+
+    public File newDirectory() {
+        File dir = new File(parentDir, nextName());
+        if (dir.mkdir()) {
+            createdDirs.add(dir);
+            return dir;
+        }
+        throw new RuntimeException("Unable to create directory: " + dir);
+    }
+
+    private String nextName() {
+        counter++;
+        return prefix + counter;
+    }
+
+    public void dispose() {
+        for (File dir : createdDirs) {
+            delete(dir);
+        }
+        createdDirs.clear();
+    }
+
+    private static void delete(File file) {
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                delete(f);
+            }
+        }
+        file.delete();
+    }
+}
---
+++ src/test/java/net/orfjackal/pommac/WorkDirectoryManagerSpec.java
@@ -0,0 +1,80 @@
+package net.orfjackal.pommac;
+
+import jdave.Specification;
+import jdave.junit4.JDaveRunner;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Esko Luontola
+ * @since 29.2.2008
+ */
+@RunWith(JDaveRunner.class)
+public class WorkDirectoryManagerSpec extends Specification<Object> {
+
+    public class CreatingAWorkDirectory {
+
+        private WorkDirectoryManager manager;
+        private File parentDir;
+
+        public Object create() {
+            parentDir = TestUtil.createWorkDir();
+            manager = new WorkDirectoryManager(parentDir);
+            return null;
+        }
+
+        public void destroy() {
+            TestUtil.deleteWorkDir();
+        }
+
+        public void atFirstThereAreNoDirectories() {
+            specify(parentDir.listFiles(), does.containExactly());
+        }
+
+        public void newDirectoriesAreAddedToTheParentDirectory() {
+            File dir = manager.newDirectory();
+            specify(parentDir.listFiles(), does.containExactly(dir));
+            specify(dir.isDirectory());
+            specify(dir.listFiles(), does.containExactly());
+        }
+
+        public void eachNewDirectoryHasAnUniqueName() {
+            File dir1 = manager.newDirectory();
+            File dir2 = manager.newDirectory();
+            specify(dir1.isDirectory());
+            specify(dir2.isDirectory());
+            specify(dir1.getName(), does.not().equal(dir2.getName()));
+            specify(parentDir.listFiles(), does.containExactly(dir1, dir2));
+        }
+
+        public void onDisposeAllCreatedDirectoriesAreDeleted() {
+            File dir = manager.newDirectory();
+            specify(dir.exists());
+            manager.dispose();
+            specify(!dir.exists());
+            specify(parentDir.exists());
+        }
+
+        public void onDisposeAllFilesInCreatedDirectoriesAreDeleted() throws IOException {
+            File dir = manager.newDirectory();
+            new File(dir, "foo.txt").createNewFile();
+            specify(dir.exists());
+            specify(dir.list(), does.containExactly("foo.txt"));
+            manager.dispose();
+            specify(!dir.exists());
+        }
+
+        public void willNotDeleteFilesOutsideTheCreatedDirectories() throws IOException {
+            File foo = new File(parentDir, "foo.txt");
+            foo.createNewFile();
+            File dir = manager.newDirectory();
+            specify(foo.exists());
+            specify(dir.exists());
+            manager.dispose();
+            specify(foo.exists());
+            specify(!dir.exists());
+        }
+    }
+}
