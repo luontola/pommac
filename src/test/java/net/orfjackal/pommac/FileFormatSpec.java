6603c7ccd75d4faddd8267010627dfe22e60e47a
Esko Luontola <esko.luontola@gmail.com>
2008-02-29 12:23:58 +0000

    switched to using jvyaml

--- src/test/java/net/orfjackal/pommac/FileFormatSpec.java
+++ src/test/java/net/orfjackal/pommac/FileFormatSpec.java
@@ -20,42 +20,42 @@ public class FileFormatSpec extends Specification<Object> {
 
         public Object create() {
             String fileText = "" +
-                    "version: sgs-src-*-r*.zip!/sgs-src-*-r* | sgs-src-(.+)-r(\\d+) >> %1$s\n" +
+                    "version: sgs-src-*-r*.zip!/sgs-src-*-r* | sgs-src-([\\d.]+)-r(\\d+) >> %1$s\n" +
                     "\n" +
                     "com.sun.sgs:\n" +
                     "    sgs:\n" +
-                    "        jar:     sgs-$VERSION-*.zip!/sgs-*/lib/sgs.jar\n" +
-                    "        javadoc: sgs-$VERSION-*.zip!/sgs-*/doc/sgs-api\n" +
+                    "        jar:     sgs-${version}-*.zip!/sgs-*/lib/sgs.jar\n" +
+                    "        javadoc: sgs-${version}-*.zip!/sgs-*/doc/sgs-api\n" +
                     "        sources:\n" +
-                    "            - sgs-src-$VERSION-*.zip!/sgs-src-*/src/server/j2se\n" +
-                    "            - sgs-src-$VERSION-*.zip!/sgs-src-*/src/shared/j2se\n" +
+                    "            - sgs-src-${version}-*.zip!/sgs-src-*/src/server/j2se\n" +
+                    "            - sgs-src-${version}-*.zip!/sgs-src-*/src/shared/j2se\n" +
                     "        depends:\n" +
-                    "            - \"berkeleydb:berkeleydb\"\n" +
-                    "            - \"org.apache.mina:mina-core\"\n" +
-                    "            - \"org.slf4j:slf4j-jdk14\"\n" +
+                    "            - berkeleydb:berkeleydb\n" +
+                    "            - org.apache.mina:mina-core\n" +
+                    "            - org.slf4j:slf4j-jdk14\n" +
                     "\n" +
                     "    sgs-client:\n" +
-                    "        jar:     sgs-client-$VERSION-*.zip!/sgs-client-*/lib/sgs-client.jar\n" +
-                    "        javadoc: sgs-client-$VERSION-*.zip!/sgs-client-*/doc/sgs-client-api\n" +
+                    "        jar:     sgs-client-${version}-*.zip!/sgs-client-*/lib/sgs-client.jar\n" +
+                    "        javadoc: sgs-client-${version}-*.zip!/sgs-client-*/doc/sgs-client-api\n" +
                     "        sources:\n" +
-                    "            - sgs-client-src-$VERSION-*.zip!/sgs-client-src-*/src/client/j2se\n" +
-                    "            - sgs-client-src-$VERSION-*.zip!/sgs-client-src-*/src/shared/j2se\n" +
+                    "            - sgs-client-src-${version}-*.zip!/sgs-client-src-*/src/client/j2se\n" +
+                    "            - sgs-client-src-${version}-*.zip!/sgs-client-src-*/src/shared/j2se\n" +
                     "        depends:\n" +
-                    "            - \"org.apache.mina:mina-core\"\n" +
-                    "            - \"org.slf4j:slf4j-jdk14\"\n" +
+                    "            - org.apache.mina:mina-core\n" +
+                    "            - org.slf4j:slf4j-jdk14\n" +
                     "\n" +
                     "berkeleydb:\n" +
                     "    berkeleydb:\n" +
-                    "        jar:     sgs-$VERSION-*.zip!/bdb-*/db.jar\n" +
-                    "        version: sgs-$VERSION-*.zip!/bdb-* | bdb-(.+) >> %1$s\n" +
+                    "        jar:     sgs-${version}-*.zip!/bdb-*/db.jar\n" +
+                    "        version: sgs-${version}-*.zip!/bdb-* | bdb-([\\d.]+) >> %1$s\n" +
                     "\n" +
                     "org.apache.mina:\n" +
                     "    mina-core:\n" +
-                    "        mvn:     sgs-$VERSION-*.zip!/mina-*/mina-core-*.jar\n" +
+                    "        mvn:     sgs-${version}-*.zip!/mina-*/mina-core-*.jar\n" +
                     "\n" +
                     "org.slf4j:\n" +
                     "    slf4j-jdk14:\n" +
-                    "        mvn:     sgs-$VERSION-*.zip!/slf4j-*/slf4j-jdk14-*.jar\n" +
+                    "        mvn:     sgs-${version}-*.zip!/slf4j-*/slf4j-jdk14-*.jar\n" +
                     "";
             Object data = YAML.load(fileText);
             artifacts = Pommac.parse(data);
@@ -80,9 +80,9 @@ public class FileFormatSpec extends Specification<Object> {
 
         public void willReadVersion() {
             Map<String, String> expected = new HashMap<String, String>();
-            expected.put("sgs", "sgs-src-*-r*.zip!/sgs-src-*-r* | sgs-src-(.+)-r(\\d+) >> %1$s");
-            expected.put("sgs-client", "sgs-src-*-r*.zip!/sgs-src-*-r* | sgs-src-(.+)-r(\\d+) >> %1$s");
-            expected.put("berkeleydb", "sgs-$VERSION-*.zip!/bdb-* | bdb-(.+) >> %1$s");
+            expected.put("sgs", "sgs-src-*-r*.zip!/sgs-src-*-r* | sgs-src-([\\d.]+)-r(\\d+) >> %1$s");
+            expected.put("sgs-client", "sgs-src-*-r*.zip!/sgs-src-*-r* | sgs-src-([\\d.]+)-r(\\d+) >> %1$s");
+            expected.put("berkeleydb", "sgs-${version}-*.zip!/bdb-* | bdb-([\\d.]+) >> %1$s");
             for (Artifact artifact : artifacts) {
                 specify(artifact.version, does.equal(expected.get(artifact.artifactId)));
             }
@@ -90,9 +90,9 @@ public class FileFormatSpec extends Specification<Object> {
 
         public void willReadJar() {
             Map<String, String> expected = new HashMap<String, String>();
-            expected.put("sgs", "sgs-$VERSION-*.zip!/sgs-*/lib/sgs.jar");
-            expected.put("sgs-client", "sgs-client-$VERSION-*.zip!/sgs-client-*/lib/sgs-client.jar");
-            expected.put("berkeleydb", "sgs-$VERSION-*.zip!/bdb-*/db.jar");
+            expected.put("sgs", "sgs-${version}-*.zip!/sgs-*/lib/sgs.jar");
+            expected.put("sgs-client", "sgs-client-${version}-*.zip!/sgs-client-*/lib/sgs-client.jar");
+            expected.put("berkeleydb", "sgs-${version}-*.zip!/bdb-*/db.jar");
             for (Artifact artifact : artifacts) {
                 specify(artifact.jar, does.equal(expected.get(artifact.artifactId)));
             }
@@ -101,11 +101,11 @@ public class FileFormatSpec extends Specification<Object> {
         public void willReadSources() {
             Map<String, String[]> expected = new HashMap<String, String[]>();
             expected.put("sgs", new String[]{
-                    "sgs-src-$VERSION-*.zip!/sgs-src-*/src/server/j2se",
-                    "sgs-src-$VERSION-*.zip!/sgs-src-*/src/shared/j2se"});
+                    "sgs-src-${version}-*.zip!/sgs-src-*/src/server/j2se",
+                    "sgs-src-${version}-*.zip!/sgs-src-*/src/shared/j2se"});
             expected.put("sgs-client", new String[]{
-                    "sgs-client-src-$VERSION-*.zip!/sgs-client-src-*/src/client/j2se",
-                    "sgs-client-src-$VERSION-*.zip!/sgs-client-src-*/src/shared/j2se"});
+                    "sgs-client-src-${version}-*.zip!/sgs-client-src-*/src/client/j2se",
+                    "sgs-client-src-${version}-*.zip!/sgs-client-src-*/src/shared/j2se"});
             expected.put("berkeleydb", new String[0]);
             expected.put("mina-core", new String[0]);
             expected.put("slf4j-jdk14", new String[0]);
@@ -116,8 +116,8 @@ public class FileFormatSpec extends Specification<Object> {
 
         public void willReadJavadoc() {
             Map<String, String> expected = new HashMap<String, String>();
-            expected.put("sgs", "sgs-$VERSION-*.zip!/sgs-*/doc/sgs-api");
-            expected.put("sgs-client", "sgs-client-$VERSION-*.zip!/sgs-client-*/doc/sgs-client-api");
+            expected.put("sgs", "sgs-${version}-*.zip!/sgs-*/doc/sgs-api");
+            expected.put("sgs-client", "sgs-client-${version}-*.zip!/sgs-client-*/doc/sgs-client-api");
             expected.put("berkeleydb", null);
             for (Artifact artifact : artifacts) {
                 specify(artifact.javadoc, does.equal(expected.get(artifact.artifactId)));
